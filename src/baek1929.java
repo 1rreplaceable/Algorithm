f28f0dd9a09ee18e10edc12195fb414345e2c80d
Seok Jin <97454046+1rreplaceable@users.noreply.github.com>
2023-10-17 16:14:39 +0900

    sol

---
+++ src/baek1929.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class baek1929 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        for(int i=A; i <=B; i++){
+            if(sosu(i)){
+                bw.write(i+"\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    static boolean sosu(int k){
+        int a =0;
+        for(int i=1; i<=k/2; i++){
+            if(k%i==0){
+                a++;
+            }
+        }
+        if(a==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
---
+++ src/baek4134.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class baek4134 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long N = Long.parseLong(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            long ans = 0;
+            long k = Integer.parseInt(br.readLine());
+            ans = sosu(k);
+            bw.write(ans+"\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static long sosu(long k){
+        int a =0;
+        for(long i=2; i<k/2; i++){
+            if(k%i==0){
+                a++;
+            }
+        }
+        if(a==0){
+            return k;
+        }else{
+            return sosu(k+1);
+        }
+    }
+}
