68cd0378a19747b9469675fdcf633e7d0d9bc507
Seok Jin <97454046+1rreplaceable@users.noreply.github.com>
2023-10-20 14:56:51 +0900

    sol

---
+++ src/baek17298.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class baek17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arrN = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i <N; i ++){
+
+            arrN[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i =0; i < arrN.length-1; i++){
+            int ans = arrN[i];
+            int q = -1;
+            for(int j = i+1; j < arrN.length; j++){
+                if(arrN[j] > ans){
+                    q = arrN[j];
+                    break;
+                }
+            }
+            bw.write(q+" ");
+        }
+        bw.write("-1");
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+
+
+}
