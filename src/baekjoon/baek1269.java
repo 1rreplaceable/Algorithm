5f0be708571794e0f9b36721a2baa94b3fc32a1a
seokjin <seokjin1205@naver.com>
2024-10-28 16:37:09 +0900

    sol

---
+++ src/baekjoon/baek1269.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class baek1269 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> setA = new HashSet<>();
+        HashSet<Integer> setB = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            setA.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            setB.add(Integer.parseInt(st.nextToken()));
+        }
+
+        HashSet<Integer> union = new HashSet<>(setA);
+        union.addAll(setB);
+
+        HashSet<Integer> intersection = new HashSet<>(setA);
+        intersection.retainAll(setB);
+
+        union.removeAll(intersection);
+
+        System.out.println(union.size());
+    }
+}
